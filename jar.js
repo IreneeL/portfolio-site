fbc0945c2938e197eaa620692b5edace2d38ad40
IreneeL <yl6447@nyu.edu>
2020-05-06 03:25:54 -0400

    added modal img code w/ different popup img

--- jar.js
+++ jar.js
@@ -6,7 +6,7 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = this.src;
+  modalImg.src = "pic/icon.png";
   captionText.innerHTML = this.alt;
 }
 
@@ -15,5 +15,25 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+ modal.style.display = "none";
+}
+// ``````````````````````````````````````````````````````````````````````````
+var modal1 = document.getElementById("myModal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("myImg1");
+var modalImg1 = document.getElementById("img011");
+var captionText = document.getElementById("caption1");
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg1.src = "pic/icon.png";
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+ modal1.style.display = "none";
 }
